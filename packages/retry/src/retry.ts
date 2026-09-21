83a8a4383899550acf153325bdef23213a577b9f
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2026-09-21 22:05:01 +0200

    use modern ecmascript syntax

--- packages/retry/src/retry.ts
+++ packages/retry/src/retry.ts
@@ -8,11 +8,11 @@ export interface RetryOptions {
 }
 
 export class Retry {
-  private readonly _retryLimit: number;
-  private readonly _timeLimit: number;
-  private readonly _delayer: Delayer;
-  private readonly _started: number = Retry.now();
-  private _attempts = 1;
+  readonly #retryLimit: number;
+  readonly #timeLimit: number;
+  readonly #delayer: Delayer;
+  readonly #started: number = Retry.now();
+  #attempts = 1;
 
   constructor(options: RetryOptions) {
     const { retryLimit = 0, timeLimit = 0, delayer } = options;
@@ -25,28 +25,28 @@ export class Retry {
     if (retryLimit === 0 && timeLimit === 0) {
       throw new Error(`Unlimited retry`);
     }
-    this._retryLimit = retryLimit;
-    this._timeLimit = timeLimit;
-    this._delayer = delayer;
+    this.#retryLimit = retryLimit;
+    this.#timeLimit = timeLimit;
+    this.#delayer = delayer;
   }
 
   get attempts(): number {
-    return this._attempts;
+    return this.#attempts;
   }
 
   get elapsed(): number {
-    return Retry.now() - this._started;
+    return Retry.now() - this.#started;
   }
 
   async tryAgain(): Promise<boolean> {
-    if (this._retryLimit > 0 && this.attempts >= this._retryLimit) {
+    if (this.#retryLimit > 0 && this.attempts >= this.#retryLimit) {
       return false;
     }
-    if (this._timeLimit > 0 && this.elapsed >= this._timeLimit) {
+    if (this.#timeLimit > 0 && this.elapsed >= this.#timeLimit) {
       return false;
     }
-    await pause(this._delayer.nextDelay(this._attempts));
-    this._attempts += 1;
+    await pause(this.#delayer.nextDelay(this.#attempts));
+    this.#attempts += 1;
     return true;
   }
 
